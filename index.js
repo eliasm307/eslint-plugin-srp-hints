36cb9f90d26fd9adfb23ff11f5fe474992bbf255
Eli <13420359+eliasm307@users.noreply.github.com>
2021-01-22 00:26:15 +0100

    plugin only exports configs and rules, tested and working locally

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
 	configs: require('./lib/configs'),
 	rules: require('./lib/rules'),
-	utils: require('./lib/utils'),
-	defaultOptions: require('./lib/defaultOptions'),
+	// utils: require('./lib/utils'), // ? remove
+	// defaultOptions: require('./lib/defaultOptions'), // ? remove
 };
